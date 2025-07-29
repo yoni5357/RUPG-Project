6b8eb5a1e805edcf1583a1b87d07bb6f47369b3a
Yonatan Gorovetz <goro5357@gmail.com>
2025-07-29 16:44:12 +0300

    added fetchPokemone to dataProvider

--- dataProvider.js
+++ dataProvider.js
@@ -1,19 +1,53 @@
 
 async function fetchtUsers() {
-    const response = await fetch('https://randomuser.me/api/?results=7');
-    if(!response.ok){
-        throw new Error('Failed to fetch users');
+    try{
+        const response = await fetch('https://randomuser.me/api/?results=7');
+        if(!response.ok){
+            throw new Error('Response not ok');
+        }
+        const users = await response.json();
+        return users.results;
+    }catch(error){
+        console.error("Failed to fetch users: ", error.message);
     }
-    const users = await response.json();
-    return users.results;
+
 }
 
 
 async function fetchQuote() {
-    const response = await fetch('https://api.kanye.rest');
-    if(!response.ok){
-        throw new Error('Faild to fetch quote')
+    try{
+        const response = await fetch('https://api.kanye.rest');
+        if(!response.ok){
+            throw new Error('Response not ok')
+        }
+        const quote = await response.json();
+        return quote.quote;
+    }catch(error){
+        console.error("Failed to fetch quote: ", error.message);
     }
-    const quote = await response.json();
-    return quote.quote;
+
+}
+
+
+async function fetchPokemone() {
+    try{
+        const countResponse = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=1');
+        if(!countResponse.ok){
+            throw new Error('Count Response not ok');
+        }
+        const pokemoneCount = await countResponse.json();
+        const totalPokemone =  pokemoneCount.count;
+
+        const randomId = Math.floor(Math.random() * totalPokemone) + 1;
+
+        const pokemoneResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
+        if(!pokemoneResponse.ok){
+            throw new Error('Pokemone Response not ok');
+        }
+        const pokemone = pokemoneResponse.json();
+        return pokemone;
+    }catch(error){
+        console.error("Failed to fetch Pokemone: ", error.message);
+    }
+
 }
