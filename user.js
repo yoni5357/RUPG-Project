4014938819cd00a14ebc08b4107ad22b300bb358
Yonatan Gorovetz <goro5357@gmail.com>
2025-07-29 17:37:20 +0300

    created the user class to hold all user data

---
+++ user.js
@@ -0,0 +1,15 @@
+class User{
+    constructor(firstName, lastName, city, state, imageUrl, quote, pokemoneName, pokemoneUrl, ipsum){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.profileImage = imageUrl;
+        this.quote = quote;
+        this.pokemoneName = pokemoneName;
+        this.pokemoneUrl = pokemoneUrl;
+        this.ipsum = ipsum;
+    }
+}
+
+export default User;
