09bb60e429fbb8b39fa0b463b48b394046019414
Yonatan Gorovetz <goro5357@gmail.com>
2025-07-29 16:11:00 +0300

    added fetchQuote to dataProvider

--- dataProvider.js
+++ dataProvider.js
@@ -2,8 +2,18 @@
 async function fetchtUsers() {
     const response = await fetch('https://randomuser.me/api/?results=7');
     if(!response.ok){
-        throw new Error('Failed fetching users');
+        throw new Error('Failed to fetch users');
     }
     const users = await response.json();
     return users.results;
+}
+
+
+async function fetchQuote() {
+    const response = await fetch('https://api.kanye.rest');
+    if(!response.ok){
+        throw new Error('Faild to fetch quote')
+    }
+    const quote = await response.json();
+    return quote.quote;
 }
