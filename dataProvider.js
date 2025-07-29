1da7df8cf6045bb2089edcc40d69ecb45b3aaa94
Yonatan Gorovetz <goro5357@gmail.com>
2025-07-29 17:10:22 +0300

    changed dataProvider to a utility class with static functions

--- dataProvider.js
+++ dataProvider.js
@@ -1,68 +1,72 @@
+class DataProvider{
 
-async function fetchtUsers() {
-    try{
-        const response = await fetch('https://randomuser.me/api/?results=7');
-        if(!response.ok){
-            throw new Error('Response not ok');
+    static async fetchtUsers() {
+        try{
+            const response = await fetch('https://randomuser.me/api/?results=7');
+            if(!response.ok){
+                throw new Error('Response not ok');
+            }
+            const users = await response.json();
+            return users.results;
+        }catch(error){
+            console.error("Failed to fetch users: ", error.message);
         }
-        const users = await response.json();
-        return users.results;
-    }catch(error){
-        console.error("Failed to fetch users: ", error.message);
+
     }
 
-}
 
-
-async function fetchQuote() {
-    try{
-        const response = await fetch('https://api.kanye.rest');
-        if(!response.ok){
-            throw new Error('Response not ok')
+    static async fetchQuote() {
+        try{
+            const response = await fetch('https://api.kanye.rest');
+            if(!response.ok){
+                throw new Error('Response not ok')
+            }
+            const quote = await response.json();
+            return quote.quote;
+        }catch(error){
+            console.error("Failed to fetch quote: ", error.message);
         }
-        const quote = await response.json();
-        return quote.quote;
-    }catch(error){
-        console.error("Failed to fetch quote: ", error.message);
+
     }
 
-}
 
+    static async fetchPokemone() {
+        try{
+            const countResponse = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=1');
+            if(!countResponse.ok){
+                throw new Error('Count Response not ok');
+            }
+            const pokemoneCount = await countResponse.json();
+            const totalPokemone =  pokemoneCount.count;
 
-async function fetchPokemone() {
-    try{
-        const countResponse = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=1');
-        if(!countResponse.ok){
-            throw new Error('Count Response not ok');
+            const randomId = Math.floor(Math.random() * totalPokemone) + 1;
+
+            const pokemoneResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
+            if(!pokemoneResponse.ok){
+                throw new Error('Pokemone Response not ok');
+            }
+            const pokemone = pokemoneResponse.json();
+            return pokemone;
+        }catch(error){
+            console.error("Failed to fetch Pokemone: ", error.message);
         }
-        const pokemoneCount = await countResponse.json();
-        const totalPokemone =  pokemoneCount.count;
 
-        const randomId = Math.floor(Math.random() * totalPokemone) + 1;
-
-        const pokemoneResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
-        if(!pokemoneResponse.ok){
-            throw new Error('Pokemone Response not ok');
-        }
-        const pokemone = pokemoneResponse.json();
-        return pokemone;
-    }catch(error){
-        console.error("Failed to fetch Pokemone: ", error.message);
     }
 
-}
 
-
-async function fetchMeatyIpsum() {
-    try{
-        const response = await fetch('https://baconipsum.com/api/?type=all-meat&paras=1');
-        if(!response.ok){
-            throw new Error('Ipsum Response not ok')
+    static async fetchMeatyIpsum() {
+        try{
+            const response = await fetch('https://baconipsum.com/api/?type=all-meat&paras=1');
+            if(!response.ok){
+                throw new Error('Ipsum Response not ok')
+            }
+            const ipsum = await response.json();
+            return ipsum[0];
+        }catch(error){
+            console.error("Failed to fetch Ipsum: ", error.message);
         }
-        const ipsum = await response.json();
-        return ipsum[0];
-    }catch(error){
-        console.error("Failed to fetch Ipsum: ", error.message);
-    }
 
+    }
 }
+
+export default DataProvider;
